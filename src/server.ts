94af95a15e8d7f5fa9ecb968bcec4d349f390dfd
henriquepe <henrique.p.espindola@gmail.com>
2020-09-27 15:19:00 -0300

    preparing deploy

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
-import * as express from 'express';
-import * as mongoose from 'mongoose';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
 import sessions from './routes/sessions.routes';
 import signup from './routes/signup.routes';
+
 import usersRouter from './routes/users.routes';
 
 // app
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const uri =
   'mongodb+srv://rosa1702:rosa1702@cluster0.m7oe2.mongodb.net/skydb?retryWrites=true&w=majority';
